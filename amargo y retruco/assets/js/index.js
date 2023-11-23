7a6532176395c4802e65863e188c96a86483015e
Elmisimismisimo <127501153+Elmisimismisimo@users.noreply.github.com>
2023-11-23 03:17:01 -0300

    Add files via upload

--- amargo y retruco/assets/js/index.js
+++ amargo y retruco/assets/js/index.js
@@ -31,7 +31,7 @@ class sprite{
         this.draw()
       }
     }  
-            //peleadores 
+         
     const fondito=new sprite({
         position:{
             x:0,
@@ -43,11 +43,11 @@ class sprite{
 
 
     class tramontina extends sprite { 
-        constructor({position, velosity, color='blue', place, foto, escala=1, maximo=1,cuadrado,sprites})
+        constructor({position, velosity, color='blue', place, foto, escala=1, maximo=1,cuadrado,sprites,ataque={place:{},width:undefined,height: undefined } })
         { 
-            
+             
             super({
-                position,foto,escala,maximo,cuadrado
+                position,foto,escala,maximo,cuadrado,place
     
                 })
             this.velosity = velosity
@@ -58,9 +58,9 @@ class sprite{
                     x:this.position.x,
                     y:this.position.y
                 },
-                place,
-                    width:100,
-                    height:50  
+                place: ataque.place,
+                    width:ataque.width,
+                    height:ataque.height 
             }
             this.color=color
             this.facaso
@@ -69,17 +69,17 @@ class sprite{
             this.pasado=0
             this.mantener=5
             this.sprites=sprites
+            this.muerte=false
+            
 
             for (const sprite in this.sprites){
                 sprites[sprite].imagen=new Image()
                 sprites[sprite].imagen.src= sprites[sprite].foto
             } 
-        
-         
-
         }
         animaciones(){
             this.pasado++
+
 
             if (this.pasado % this.mantener===0){
                 if(this.actual<this.maximo-1){
@@ -93,26 +93,110 @@ class sprite{
         update(){
             this.draw()
             this.animaciones()
+            if(!this.muerte) this.cambiazo()
             
             this.ataque.position.x=this.position.x +this.ataque.place.x
-            this.ataque.position.y=this.position.y 
+            this.ataque.position.y=this.position.y +this.ataque.place.y
+
+           
             this.position.x += this.velosity.x
             this.position.y += this.velosity.y
     
             if (this.position.y + this.height + this.velosity.y >= canvas.height -50){
                 this.velosity.y=0
+                this.position.y= 376
             }else
             { (this.velosity.y +=gravedad)}
-          }
+            
+        }
     
             golpe(){
+                this.cambiazo('attack')
                 this.facaso=true
                 setTimeout(() => {
                     this.facaso=false
-                },100);
+                },100)
             }
-        }
+            golpeado(){
+                
+                this.vida-=10
+                if(this.vida<=0){
+                this.cambiazo('muerte')
+                }
+                else (this.cambiazo('golpeado'))
+            }
+
+
+            cambiazo(sprite){
+                if(this.imagen===this.sprites.muerte.imagen){ 
+                    if(this.actual=this.sprites.muerte.maximo-1)this.muerte=true
+                    return}
+
+                if(this.imagen===this.sprites.attack.imagen&&this.actual<this.sprites.attack.maximo-1)
+                return
+            if(this.imagen===this.sprites.golpeado.imagen&&this.actual<this.sprites.golpeado.maximo -1)
+            return
+                switch(sprite){
+                    case 'idle':
+                        if(this.imagen !== this.sprites.idle.imagen){
+                        this.imagen=this.sprites.idle.imagen
+                        this.maximo= this.sprites.idle.maximo
+                        this.actual=0}
+                        break
+                     case'run':
+                        if (this.imagen!==this.sprites.run.imagen){
+                            this.imagen= this.sprites.run.imagen
+                            this.maximo= this.sprites.run.maximo
+                            this.actual=0
+                        }
+                        break
+                     case 'jump':
+                        if (this.imagen!==this.sprites.jump.imagen){
+                        this.imagen= this.sprites.jump.imagen
+                        this.maximo= this.sprites.jump.maximo
+                        this.actual=0
+                        } break
+
+                     case 'fall':
+                            if (this.imagen!==this.sprites.fall.imagen){
+                            this.imagen= this.sprites.fall.imagen
+                            this.maximo= this.sprites.fall.maximo
+                            this.actual=0
+                            }break
+                     case 'attack':
+                                if (this.imagen!==this.sprites.attack.imagen){
+                                this.imagen= this.sprites.attack.imagen
+                                this.maximo= this.sprites.attack.maximo
+                                this.actual=0
+                                }
+                                break
+
+                    case 'golpeado':
+                                    if (this.imagen!==this.sprites.golpeado.imagen){
+                                    this.imagen= this.sprites.golpeado.imagen
+                                    this.maximo= this.sprites.golpeado.maximo
+                                    this.actual=0
+                                    }
+                                    break
+                     
+                    case 'muerte':
+                                        if (this.imagen!==this.sprites.muerte.imagen){
+                                        this.imagen= this.sprites.muerte.imagen
+                                        this.maximo= this.sprites.muerte.maximo
+                                        this.actual=0
+                                        }
+                                        break
     
+                        
+    
+
+                      
+                                
+                }
+            }
+        
+    }
+       
 
 const jugador=new tramontina({
     position:{
@@ -143,12 +227,36 @@ place:{
             foto: '../img/Sprites/Run.png',
               maximo:8
         },
-    jump:{
+       jump:{
         foto:'../img/Sprites/Jump.png',
         maximo:2
-
+ 
+       },
+       fall:{
+        foto:'../img/Sprites/Fall.png',
+        maximo:2
+       },
+        attack:{
+        foto:'../img/Sprites/Ataque.png',
+        maximo: 6 
+    },
+        golpeado: {
+            foto:'../img/Sprites/Take Hit - white silhouette.png',
+            maximo:4},
+            muerte: {
+                foto:'../img/Sprites/Death.png',
+                maximo:6
+            }
+    },
+    ataque: {
+        place:{
+            x:135,
+            y:30
+        },
+        width:170,
+        height:50
     }
-    }
+   
 
 }) 
 
@@ -166,7 +274,57 @@ color:"purple",
 place:{
     x:-50,
     y:0
+    },
+    foto: '../img/Sprites2/Idle.png',
+    maximo:8,
+    escala:3, 
+
+    cuadrado:{
+        x:215 ,y:180
+    },
+    sprites: {
+        idle:{
+            foto: '../img/Sprites2/Idle.png',
+             maximo: 8
+        },
+        run:{
+            foto: '../img/Sprites2/Run.png',
+              maximo:8
+        },
+       jump:{
+        foto:'../img/Sprites2/Jump.png',
+        maximo:2
+ 
+       },
+       fall:{
+        foto:'../img/Sprites2/Fall.png',
+        maximo:2
+       },
+        attack:{
+        foto:'../img/Sprites2/Ataque2.png',
+        maximo: 6 },
+        golpeado: {
+            foto:'../img/Sprites2/Take Hit - white silhouette.png',
+            maximo:4
+    
+        },
+        muerte: {
+            foto:'../img/Sprites2/Death.png',
+            maximo:6
+        }
+    
+    },
+    
+
+    ataque: {
+        place:{
+            x:-255,
+            y:30
+        },
+        width:170,
+        height:50
     }
+
 }) 
 //movimiento 
  const keys={
@@ -210,9 +368,7 @@ function ganador({jugador, jugador2})
     }
 
 
-function correr(){
-    jugador.imagen=jugador.sprites.run.imagen
-}
+
 
  function animar(){
     window.requestAnimationFrame(animar);
@@ -220,48 +376,67 @@ function correr(){
     a.fillRect(0,0, canvas.width,canvas.height)
     fondito.update()
     jugador.update()
-    //jugador2.update()
+    jugador2.update()
     jugador.velosity.x=0
     jugador2.velosity.x=0
-    jugador.imagen=jugador.sprites.idle.imagen
+
+
+    
     if (keys.d.pressed  &&jugador.lastkey==="d"){ 
         jugador.velosity.x=5
-        correr()
+        jugador.cambiazo('run')
     }
     else if (keys.a.pressed &&jugador.lastkey==="a"){
     jugador.velosity.x=-5
-    correr()
-   }
+    jugador.cambiazo('run')
+}else{jugador.cambiazo('idle')}
+   
+
    if(jugador.velosity.y<0){
-    jugador.imagen=jugador.sprites.jump.imagen
-    jugador.maximo=jugador.sprites.jump.maximo
-   }
+    jugador.cambiazo('jump')
+   }else  if(jugador.velosity.y > 0){
+    jugador.cambiazo('fall')}
+
     
 
 if (keys.ArrowRight.pressed  &&jugador2.lastkey==="ArrowRight"){ 
-    jugador2.velosity.x=5}
+    jugador2.velosity.x=5
+    jugador2.cambiazo('run')}
+    
 
 else if (keys.ArrowLeft.pressed &&jugador2.lastkey==="ArrowLeft"){
 jugador2.velosity.x=-5
-}
+jugador2.cambiazo('run')}
+else{jugador2.cambiazo('idle')}
+
+if(jugador2.velosity.y<0){
+    jugador2.cambiazo('jump')
+   }else  if(jugador2.velosity.y > 0){
+    jugador2.cambiazo('fall')}
+
+
 
 if (colicion({rect1: jugador,rect2: jugador2})&&jugador.facaso)
     {
+        jugador2.golpeado()
         jugador.facaso=false
         jugador2.vida-=10
+        
         document.querySelector('#vida2').style.width=jugador2.vida+'%'
+        
    console.log("jugador 1 HIT")
+
    if (jugador.vida<=0||jugador2.vida<=0){
     ganador({jugador,jugador2})
-
-    
    }
 
 }
+
 if (colicion({rect1: jugador2, rect2: jugador})&&jugador2.facaso)
     {
+        jugador.golpeado()
         jugador2.facaso=false
-        jugador.vida-=10
+        
         document.querySelector('#vida1').style.width=jugador.vida+'%'
         if (jugador.vida<=0||jugador2.vida<=0){
             ganador({jugador,jugador2})
@@ -270,9 +445,12 @@ if (colicion({rect1: jugador2, rect2: jugador})&&jugador2.facaso)
    console.log("jugador 2 HIT")
    
 }
+
 }
 animar() 
  window.addEventListener("keydown", (envento)=>{
+    if (!jugador.muerte){
+
     switch (envento.key){
         case "d":
             keys.d.pressed=true
@@ -289,7 +467,9 @@ animar()
             jugador.golpe()
             break
 
-
+    }}
+    if (!jugador2.muerte ){
+        switch(envento.key){
             case "ArrowRight":
             keys.ArrowRight.pressed=true
             jugador2.lastkey="ArrowRight"
@@ -306,6 +486,7 @@ animar()
                 break
             
     }
+}
 
  })
  window.addEventListener("keyup", (envento)=>{
